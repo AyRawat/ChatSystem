24aa7a5c918eebfa38f7630826fae401e845b8ef
VADER1998 <aayushrawat20@gmail.com>
2019-04-24 23:23:31 +0530

    Hello

---
+++ ChatApp/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(4000,function(){
+  console.log('listening @4000');
+});
+
+app.use(express.static('public'));
+
+
+var io = socket(server);
+
+io.on('connection',function(socket){
+  console.log('made socket connection',socket.id);
+  socket.on('chat',function(data){
+    io.sockets.emit('chat',data);
+  })
+});
